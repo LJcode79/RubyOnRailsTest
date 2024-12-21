b4864485e520cf033d9e534495d944a1474015b1
LJcode79 <ljdb1999@gmail.com>
2024-12-20 19:59:17 -0800

    lint fix attempt whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get "pages/home"
   # get "pages/about"
   get "about", to: "pages#about"
-  get "home", to: "pages#home" 
+  get "home", to: "pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
